25352dc6108119f8c2990a9ef3dbb0a7fe49b8ad
anirudh.mallissery@gmail.com <anirudh.mallissery@gmail.com>
2023-11-25 09:38:08 +0000

    Edited ViewBooks.RowTemplate1 and ViewBooks

--- client_code/ViewBooks/__init__.py
+++ client_code/ViewBooks/__init__.py
@@ -11,11 +11,12 @@ class ViewBooks(ViewBooksTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.init_components(**properties)
-    self.repeating_panel_1.text = anvil.server.call('view_books')
+    if type(anvil.server.call('view_books'))
+    self.repeating_panel_1.items = anvil.server.call('view_books')
     self.label_1.text = self.item['BOOK_ID']
     self.label_2.text = self.item['CATEGORY']
     self.label_3.text = self.item['NAME']
     self.label_4.text = self.item['AUTHOR']
     self.label_5.text = self.item['COPIES']
     self.label_6.text = self.item['STATUS']
-    # Any code you write here will run before the form opens.
+   # Any code you write here will run before the form opens.
