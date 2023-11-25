d1faef463dce7911f6626ad44705057d097e22fb
anirudh.mallissery@gmail.com <anirudh.mallissery@gmail.com>
2023-11-25 08:36:11 +0000

    Edited ViewBooks.RowTemplate1 and ViewBooks

--- client_code/ViewBooks/RowTemplate1/__init__.py
+++ client_code/ViewBooks/RowTemplate1/__init__.py
@@ -10,4 +10,14 @@ class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.repeating_panel_1.text = anvil.server.call('view_books')
+    self.label_1.text = self.item[0]
+    self.label_2.text = self.item[1]
+    self.label_3.text = self.item[2]
+    self.label_4.text = self.item[3]
+    self.label_5.text = self.item[4]
+    self.label_6.text = self.item[5]
+    
+    
+    
     # Any code you write here will run before the form opens.
--- client_code/ViewBooks/__init__.py
+++ client_code/ViewBooks/__init__.py
@@ -10,5 +10,12 @@ class ViewBooks(ViewBooksTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.init_components(**properties)
     self.repeating_panel_1.text = anvil.server.call('view_books')
+    self.label_1.text = self.item['BOOK_ID']
+    self.label_2.text = self.item['CATEGORY']
+    self.label_3.text = self.item['NAME']
+    self.label_4.text = self.item['AUTHOR']
+    self.label_5.text = self.item['COPIES']
+    self.label_6.text = self.item['STATUS']
     # Any code you write here will run before the form opens.
